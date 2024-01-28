5789a59e6c6a539ab40e12fcf0a516b08c3b0d37
Bobovino <rodrigomartineztabernero97@gmail.com>
2024-01-28 22:07:06 +0100

    auto-discovering tasks in a directory

---
+++ dcelery/dcelery/celery_tasks/ex-name.py
@@ -0,0 +1,14 @@
+from dcelery.celery_config import app
+
+
+@app.task(queue='tasks')
+def my_task1():
+    pass
+
+@app.task(queue='tasks')
+def my_task2():
+    pass
+
+@app.task(queue='tasks')
+def my_task3():
+    pass
